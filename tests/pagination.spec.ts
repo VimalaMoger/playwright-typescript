4dc9afdc8642b2ab8716fa5ae53397aa215e9a6b
VimalaMoger <mgrvmla1@gmail.com>
2025-12-24 13:26:48 -0600

    pagination test

--- tests/pagination.spec.ts
+++ tests/pagination.spec.ts
@@ -1,64 +1,81 @@
 import { test, expect, Locator } from '@playwright/test';
 
-test("should navigate through pagination correctly", async ({ page }) => {
-  // Navigate to the page with pagination
-  await page.goto('https://datatables.net/examples/basic_init/zero_configuration.html');
 
-  let nextPage = true;
+test("should display correct number of entries per page", async ({ page }) => {
+
+    // Navigate to the page with pagination
+    await page.goto('https://dapper-macaron-0392da.netlify.app/page4');   
+    
+
+    // repeat the same logic for multiple pages
+      let nextPage = true;
   
 
-  while (nextPage) {
-    const rowsPerPage = await page.locator("#example tbody tr").all();
-    for (let row of rowsPerPage) {
-        console.log(await row.innerText());
-    }
-    // repeat the same logic for multiple pages
-    const nextButton =page.locator('button[aria-label="Next"]');
-    const isDisabled= await nextButton.getAttribute('class');
+    while (nextPage) {
 
-    if (isDisabled && isDisabled.includes('disabled')) {
-            nextPage = false;
-    } else {
-            await nextButton.click();
-            await page.waitForTimeout(1000); // wait for the page to load
+        const rowsPerPage = await page.locator("#tableData tbody tr").all();
+        for (let row of rowsPerPage) {
+            console.log(await row.innerText());
+            
+        }
+
+        const nextButton = page.locator("button[aria-label='Next']");
+        const isDisabled = await nextButton.getAttribute('class');
+
+        if (isDisabled?.includes('disabled')) {
+                nextPage = false;
+        } else {
+                await nextButton.click();
+                await page.waitForTimeout(1000); // wait for the page to load
+        } 
     }
-  }
+});
+
+test("should navigate through pagination correctly", async ({ page }) => {
+
+    // Navigate to the page with pagination
+    await page.goto('https://dapper-macaron-0392da.netlify.app/');
+
+    // Scrape the title
+    const name = await page.locator('#name').getAttribute('value');
+    console.log("Name:", name);
+    expect(name).toBe('John Smith');
 });
 
 test("filter the rows and check the rows count", async ({ page }) => {
 
-  // Navigate to the page with pagination
-  await page.goto('https://datatables.net/examples/basic_init/zero_configuration.html');
+    // Navigate to the page with pagination
+    await page.goto('https://dapper-macaron-0392da.netlify.app/page4');
 
-  // Select the entries dropdown and choose 25 entries
+    // Select the entries dropdown and choose 20 entries
     const dropdown: Locator = page.locator("#dt-length-0");
-    await dropdown.selectOption({label: '25'});
+    await dropdown.selectOption({label: '20'});
 
-    const rowsPerPage = await page.locator("#example tbody tr").all();
-    expect(rowsPerPage.length).toBeLessThanOrEqual(25);
+    const rowsPerPage = await page.locator("#tableData tbody tr").all();
+    expect(rowsPerPage.length).toBeLessThanOrEqual(20);
 
 });
 
 test.only("search for specific data in a table", async ({ page }) => {
 
-  // Navigate to the page with pagination
-  await page.goto('https://datatables.net/examples/basic_init/zero_configuration.html');
+    // Navigate to the page with pagination
+    await page.goto('https://dapper-macaron-0392da.netlify.app/page4');
 
-  // search name 'Ashton Cox' in the search box
+    // search name 'Brat Steve' in the search box
     const dropdown: Locator = page.locator("#dt-search-0");
     
     // Enter the search term
-    await dropdown.fill('Ashton Cox');
+    await dropdown.fill('Brat Steve');
 
     await page.waitForTimeout(1000); 
 
     // Verify that the table displays the correct results
-    const rowsPerPage = await page.locator("#example tbody tr").all();
+    const rowsPerPage = await page.locator("#tableData tbody tr").all();
     if (rowsPerPage.length > 0) {
         let matchFound = false;
         for (let row of rowsPerPage) {
             const rowText = await row.innerText();
-            if (rowText.includes('Ashton Cox')) {
+            if (rowText.includes('Brat Steve')) {
                 matchFound = true;
                 break;
             }
