d1596d6799d0225314056cdb05852c46b30fe689
VimalaMoger <mgrvmla1@gmail.com>
2025-12-28 17:00:06 -0600

    dialogs and frames

---
+++ tests/dialogs.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('dialogs handling', async ({ page }) => {
+  // Navigate to the page that triggers dialogs
+  await page.goto('https://dapper-macaron-0392da.netlify.app/');
+
+  // Go to second page
+  page.locator("//a[text()='2']").click();
+
+  page.on('dialog', (dialog) => {
+
+    console.log("Dialog type is:", dialog.type());
+    expect(dialog.type()).toBe('alert');
+    console.log(`Dialog message: ${dialog.message()}`);
+    expect(dialog.message()).toContain('Hi, I was clicked');
+    dialog.accept();
+  });
+
+  await page.locator("//a[normalize-space()='Click Me']").click();
+  await page.waitForTimeout(5000);
+
+});
