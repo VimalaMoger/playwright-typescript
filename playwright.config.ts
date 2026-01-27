55bcd11ddd9e8d687bf1eed7b9b5fe0c629fdcf7
VimalaMoger <mgrvmla1@gmail.com>
2026-01-26 23:26:28 -0600

    config file added

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,48 +12,63 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 60 * 1000,
-  testDir: './tests',
+  testDir: './tests', //./e2e',
   /* Run tests in files in parallel */
+
+  //grep:/@sanity/,
+  //grep:/@sanity/,
+  //grep:/(?=./*@grouping)(?=.*@regression)/,
+  //grepInvert:/@sanity/,
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  //retry locally
+  //retries: 3,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+      ['html', { open: 'always', outputFolder: 'html-report' }],
+      /*['line'],
+      ['json', {  outputFile: 'test-results.json' }],
+      ['junit', {  outputFile: 'test-results.xml' }],
+      ['list'], 
+      ['allure-playwright', { outputFolder: 'allure-report' }], */
+      ['./my-custom-reporter.ts'],
+      ],
+   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+
+    screenshot:'only-on-failure',
+    video: 'retain-on-failure',
+    //trace: 'on-first-retry',
+    trace: 'off',
   },
 
   /* Configure projects for major browsers */
   projects: [
-    
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      //fullyParallel: true,
     },
-  
-    /*
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    }, 
-    */
-    
-   /*
+    },
+
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },  */
-
+    },
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
