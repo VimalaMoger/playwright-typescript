576013a4b1a7230002eeb97d9f925c0949595f64
VimalaMoger <mgrvmla1@gmail.com>
2026-01-29 18:30:02 -0600

    files in a folder

---
+++ tests/visual&Accessibility/accessibilityTest.spec.ts
@@ -0,0 +1,23 @@
+import {test, expect} from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+
+test('Accessibility test for the main page', async ({page}) => {
+  // Navigate to the main page
+  await page.goto('https://kaleidoscopic-cat-fc1c3c.netlify.app/');
+
+  // Initialize AxeBuilder with the current page
+  const axeBuilder = new AxeBuilder({page});
+
+    // Analyze the page for accessibility issues
+    const results = await axeBuilder.analyze();
+    
+    // Log the results to the console
+    console.log(results);
+
+    // Assert that there are no accessibility violations
+    expect(results.violations).toEqual([]);     
+});
+
+
+
