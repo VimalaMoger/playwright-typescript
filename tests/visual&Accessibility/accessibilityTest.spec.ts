89adc43484bde0cd8ac8cfa5ec90af0e22e981b6
VimalaMoger <mgrvmla1@gmail.com>
2026-03-04 18:42:46 -0600

    comment out line

--- tests/visual&Accessibility/accessibilityTest.spec.ts
+++ tests/visual&Accessibility/accessibilityTest.spec.ts
@@ -4,19 +4,19 @@ import AxeBuilder from '@axe-core/playwright';
 
 test('Accessibility test for the main page', async ({page}, testInfo) => {
   // Navigate to the main page
-  await page.goto('https://kaleidoscopic-cat-fc1c3c.netlify.app/');
+  await page.goto('https://precious-scone-c844ed.netlify.app/');
 
   // Initialize AxeBuilder with the current page
   const axeBuilder = new AxeBuilder({page});
 
   // Analyze the page for accessibility issues- scanning to detect all types of WCAG violations
-  //const results = await axeBuilder.analyze();
+  const results = await axeBuilder.analyze();
 
   //scanning for all WCAG 2.0 and WCAG 2.1 violations
   //const results = await axeBuilder.withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']).analyze();
 
   //scanning for few WCAG voilations with disable rules
-  const results = await axeBuilder.disableRules(['aria-required-children']).withTags(['wcag2a', 'wcag2aa']).analyze();
+  //const results = await axeBuilder.disableRules(['aria-required-children']).withTags(['wcag2a', 'wcag2aa']).analyze();
 
   //attach results to test info for reporting
   await testInfo.attach('accessibility-report', {
