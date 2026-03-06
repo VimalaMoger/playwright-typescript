69c7c82d02d7f49e1971b4a0e9c4fc5bf658ce9c
VimalaMoger <mgrvmla1@gmail.com>
2026-03-06 16:20:17 -0600

    Adding comments to baseTest.ts

--- fixtures/baseTest.ts
+++ fixtures/baseTest.ts
@@ -1,10 +1,10 @@
 import { test as base, Page as page } from '@playwright/test';
 import { LoginPage } from '../pages/foodarapp/LoginPage';
 import { RegisterPage } from '../pages/foodarapp/RegisterPage';
-import { ItemsPage } from '../pages/foodarapp/itemsPage';
 import { ConfirmPage } from '../pages/foodarapp/ConfirmPage';
+import { ItemsPage } from '../pages/foodarapp/ItemsPage'
 
-// Defice custom fixtures
+// Define custom fixture
 type Fixtures = {
     loginPage: LoginPage;
     registerPage: RegisterPage;
@@ -12,11 +12,13 @@ type Fixtures = {
     confirmPage: ConfirmPage;
 };
 
-// Extend the base test with our custom fixtures
+// Extend the base test with our custom fixture
 export const test = base.extend<Fixtures>({
+    // Create loginPage object
     loginPage: async ({ page }, use) => {   
         const loginPage = new LoginPage(page);
-        await use(loginPage);
+        // Provide the object to the test
+        await use(loginPage); 
     },
     registerPage: async ({ page }, use) => {
         const registerPage = new RegisterPage(page);
