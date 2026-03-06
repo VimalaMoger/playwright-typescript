d641b8b594cd87799cf1886e22b60664c9b22fd7
VimalaMoger <mgrvmla1@gmail.com>
2026-03-05 19:58:26 -0600

    Updated test script in jsonData.spec.ts using custom fixtures(Page Object Fixtures)

--- tests/dataDriven/json_data/jsonData.spec.ts
+++ tests/dataDriven/json_data/jsonData.spec.ts
@@ -1,5 +1,7 @@
-import {test, expect, Locator, Page} from '@playwright/test';
+import { expect } from '@playwright/test';
+import {test} from '../../../fixtures/baseTest';
 import fs from 'fs';
+
 /* 
   This test script verifies the text input actions on a sample web application.
             // https://sweet-torte-0bf6bc.netlify.app/  // Sample web app URL
@@ -17,69 +19,40 @@ import fs from 'fs';
 */
 
 //test.describe.configure({ mode: 'parallel' });
+//test.use({ actionTimeout: 10000 });
 
 // Read data from JSON file
 const loginData :any= JSON.parse(fs.readFileSync('./tests/dataDriven/json_data/data.json', 'utf-8'));
 
-let page: Page;
-// approach I
-    //for (const index of loginData || []) {  // Fallback to empty array
-// approach II
+//for (const index of loginData || []) {  // Fallback to empty array if loginData is undefined or null
+
 // check if loginData is an array
 if (Array.isArray(loginData)) {    
-    for (const [email, password, firstName] of loginData.map((item: {email: string, password: string, firstName: string}) => [item.email, item.password, item.firstName])) {
-        test.beforeAll('Register',async ({browser}) => {
-            page = await browser.newPage();
-            // Navigate to url
-            await page.goto('https://sweet-torte-0bf6bc.netlify.app/');
-            
-            // Verify register button clickable
-            const registerButtonClick:Locator = page.getByRole('link', {name: 'Register' });
-            await expect(registerButtonClick).toBeVisible(); 
-            await registerButtonClick.click();
-
-            // Verify register page successfully loaded and entered data
-
-            await page.locator('#email').fill(email);
-
-            await page.locator('input[type="password"]').fill(password);
-
-            await page.locator('#fName').fill(firstName);  
-            await page.locator('input[type="submit"]').click();
-            
-        });
+    for (let [email, password, firstName] of loginData.map((item: {email: string, password: string, firstName: string}) => [item.email, item.password, item.firstName])) {
+     
 
         test.describe(`Grouping multiple tests ${firstName}`, {tag: ['@grouping','@regression']}, async () => {
-
-            test('Login', async () => {
-                const loginText:Locator = page.getByRole("heading", { name: 'Please sign in' }); //getByRole
-                await expect(loginText).toBeVisible({timeout:50000});
-
-                // Verify login page successfully loaded and entered data
-                await page.locator('#fName').fill(firstName);
-                // grab first name 
-                const enteredFirstName : string =  await page.locator('#fName').inputValue();
-                expect(enteredFirstName).toBe(firstName);
-
-                await page.locator('#email').fill(email);
-            
-
-                // grab email
-                const enteredEmail : string = await page.locator('#email').inputValue();
-                expect(enteredEmail).toBe(email);
-
-                await page.getByRole('button', { name: 'Login' }).click({timeout: 90000});
+            test.beforeEach('Register',async ({registerPage}) => {
+           
+                await registerPage.navigateTo('https://sweet-torte-0bf6bc.netlify.app/');
+                await registerPage.assertElementVisible();
+                await registerPage.clickRegisterLink();
+                await registerPage.register(email, password, firstName);                                 
             });
 
-            test('Items page', {tag: ['@pageVisible', '@regression']}, async () => {
-                await page.goto('https://sweet-torte-0bf6bc.netlify.app/foodarpages/displayitems');
-                const headingText:Locator = page.getByRole("heading", { name: 'Delicious Food Service' }); //getByRole
-                await expect(headingText).toBeVisible();
+            test('Login', {tag: ['@pageVisible', '@regression']}, async ({ registerPage, loginPage }) => {
+
+                await registerPage.handleAlertDialog();
+              
+                await loginPage.assertLoginDisplayTextVisible();
+
+                // fill the login form with registered data
+                await loginPage.login(email, password, firstName);
             });
 
-            test.skip('@browserTest @sanity Browser name test', async ({browserName}) => {
-                test.skip(browserName !== 'chromium', 'This test runs only on Chromium browsers');
-                // Test logic specific to Chromium browsers
+            test.skip('@browserTest @sanity Browser name test', async ({registerPage}) => {
+                test.skip(await registerPage.getBrowserName() !== 'chromium', 'This test runs only on Chromium browsers');
+                // Test logic specific to Chromium browser
                 console.log('This test is running on Chromium browser');
             });
 
@@ -91,76 +64,48 @@ if (Array.isArray(loginData)) {
             });
             
             // slow test
-            test('slow test example', async () => {
-                test.slow();
+            //test('slow test example', async () => {
+                //test.slow();
+            //});
+
+            //fail the test            
+            test.fail('Intentional failure test', async ({registerPage}) => {
+                await registerPage.assertElementVisible(); // This assertion will fail
             });
 
-            //fail the test
-            test.fail('Intentional failure test', async ({browser}) => {
-                // new context
-                const context = await browser.newContext();
-                // create a page
-                const parentPage = await context.newPage();
-                    expect(context.pages()[0]).toBe(5); // This assertion will fail
-            });
+            test.afterEach('Verify Text Input Actions', async ({ itemsPage, confirmPage }) => { 
+        
+                // Verify resource(items) page displayed after login
+                await itemsPage.assertElementVisible(); 
+                // Verify length of heading text on items page
+                const maxLength = await itemsPage.getHeadingTextLength();  
+                expect(maxLength).toBe(22);
+
+                const priceArr : number[] = await itemsPage.selectAllCheckboxesAndGetPrices();
+                        
+                const totalItemPrice : number = priceArr.reduce((acc, price) => acc + price, 0);
+                expect(totalItemPrice.toFixed(2)).toBe('85.91');
+
+                // Add to cart button click
+                await itemsPage.clickCartButton();
+
+                // Radio button 
+                const treatValue : number | null = await confirmPage.selectRadioButtonAndGetValue(3);
+                expect(treatValue).toBe(3);
+
+                await confirmPage.clickRequestButton();
+
+                //  Select Tip radio button to avoid alert 
+                const tipValue : number | null = await confirmPage.selectTipRadioButtonAndGetValue(0.10); 
+                expect(tipValue).toBe(0.10);
+
+                const totalTip : number = treatValue + tipValue * treatValue;
+                expect(totalTip).toBe(3.3);
+
+                await confirmPage.clickRequestButton();
+                        
+                await itemsPage.assertElementVisible(); 
+            });             
         });
-        
-
-        test.afterAll('Verify Text Input Actions', async () => { 
-        
-            // Verify resource(items) page displayed after login
-            const headingText:Locator = page.getByRole("heading", { name: 'Delicious Food Service' }); //getByRole
-            await expect(headingText).toBeVisible();
-
-            // Verify length of heading text
-            const maxLength = (await headingText.textContent())?.length;
-            expect(maxLength).toBe(22);
-            
-            // capture all the checkboxes
-            const checkboxes :Locator[]= await page.locator('input[type="checkbox"]').all();
-            let total : number = 0;
-
-            const priceArr  = await Promise.all(checkboxes.map(async (checkbox, index) => {
-                await checkbox.check({ force: true });
-                return await page.locator(`//tr[${index+1}]/td[3]`).textContent();
-            }));
-            
-            const totalItemPrice : number = priceArr.reduce((acc, price) => acc + parseFloat(price!), 0);
-            expect(totalItemPrice.toFixed(2)).toBe('85.91');
-            
-            // Checkbox actions - check the checkbox - single checkbox
-            await page.locator("//input[@name='Oven_Baked_Pastas']").check();
-
-            // assert the checkbox is checked
-            expect(page.locator("//input[@name='Oven_Baked_Pastas']")).toBeChecked();
-            
-            const itemPrice : string | null = await page.locator("tr:nth-child(1) td:nth-child(3)").textContent();
-            expect(itemPrice).toBe('12.99');
-
-            // Add to cart button click
-            await page.getByRole('button', { name: 'Add to Cart' }).click();
-
-            // Radio button 
-            expect(page.locator('//input[@value=3]')).toBeChecked();
-            const treatValue : string | null = await page.locator("//input[@value=3]").getAttribute('value');
-            expect(treatValue).toBe('3');
-
-            await page.locator("//input[@id='num']").click();
-
-            //  Check the checkbox again to avoid alert, since both options are now selected 
-            await page.locator("//input[@value=0.10]").check();
-
-            const tipValue : string | null = await page.locator('input[value="0.10"]').getAttribute('value');
-            expect(tipValue).toBe('0.10');
-
-            const totalTip : number = parseFloat(treatValue!) + parseFloat(tipValue!) * parseFloat(treatValue!);
-            expect(totalTip).toBe(3.3);
-
-            await page.locator("//input[@id='num']").click();
-            await page.waitForTimeout(3000);
-            
-            const headingTextt:Locator = page.getByRole("heading", { name: 'Delicious Food Service' }); //getByRole
-            await expect(headingTextt).toBeVisible();
-        }); 
     }
 }
