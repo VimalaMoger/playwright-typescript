4d9294e293ded69807c793a5a12a8a5c9ced6d07
VimalaMoger <mgrvmla1@gmail.com>
2026-03-06 20:24:30 -0600

    format fix

--- tests/dataDriven/csv/csvData.spec.ts
+++ tests/dataDriven/csv/csvData.spec.ts
@@ -3,22 +3,6 @@ import {expect} from '@playwright/test';
 import fs from 'fs';
 import { parse } from 'csv-parse/sync';
 
-/* 
-  This test script verifies the text input actions on a sample web application.
-            // https://sweet-torte-0bf6bc.netlify.app/  // Sample web app URL
-  It performs the following actions:
-  1. Navigates to the registration page.  
-  2. Fills out the registration form with sample data.
-  3. Submits the registration form and handles the alert dialog.
-  4. Verifies that the login page is displayed after registration.
-  5. Fills out the login form with the registered data.
-  6. Submits the login form.
-  7. Verifies that the items(resource) page is displayed after login.
-  8. Checks the length of the heading text on the items page.
-  9. Radio button and checkbox actions 
-  10. Handles multiple alert dialogs and verifies their messages.
-*/
-
 //test.describe.configure({ mode: 'parallel' });
 
 // Read CSV data function - read data from csv file
@@ -34,6 +18,7 @@ function readCSVData() : Array<Record<string, string>> {
 for ( const csvData of readCSVData()) { 
 
     test.describe(`Grouping multiple tests ${csvData.firstName}`, {tag: ['@grouping','@regression']}, async () => {
+
         test.beforeEach('Register',async ({registerPage}) => {
             await registerPage.navigateTo('https://sweet-torte-0bf6bc.netlify.app/');
             await registerPage.assertElementVisible();
@@ -42,41 +27,34 @@ for ( const csvData of readCSVData()) {
         });
 
         test('Login', {tag: ['@pageVisible', '@regression']}, async ({ registerPage, loginPage }) => {
-
             await registerPage.handleAlertDialog();              
             await loginPage.assertLoginDisplayTextVisible();
-            // fill the login form with registered data
-            await loginPage.login(csvData.email, csvData.password, csvData.firstName);
+            await loginPage.login(csvData.email, csvData.firstName);
         });
 
         test.skip('@browserTest @sanity Browser name test', async ({loginPage}) => {
             test.skip(await loginPage.getBrowserName() !== 'chromium', 'This test runs only on Chromium browsers');
-            // Test logic specific to Chromium browsers
             console.log('This test is running on Chromium browser');
         });
 
         //fixme the test
         test.fixme('Fixme test example', async () => {
             // This test is marked as 'fixme' and will be skipped
-            // until the underlying issue is resolved.
-            // Test logic goes here
         });
             
-        // slow test
         //test('slow test example', async () => {
             //test.slow();
         //});
 
-        //fail the test            
         test.fail('Intentional failure test', async ({registerPage}) => {
-            await registerPage.assertElementVisible(); // This assertion will fail
+            await registerPage.assertElementVisible(); 
         });        
     
         test.afterEach('Verify Text Input Actions', async ({ itemsPage, confirmPage }) => { 
         
-            // Verify resource(items) page displayed after login
+            // Verify display text
             await itemsPage.assertElementVisible(); 
-            // Verify length of heading text on items page
+            // Verify length of the text
             const maxLength = await itemsPage.getHeadingTextLength();  
             expect(maxLength).toBe(22);
 
@@ -84,8 +62,6 @@ for ( const csvData of readCSVData()) {
                         
             const totalItemPrice : number = priceArr.reduce((acc, price) => acc + price, 0);
             expect(totalItemPrice.toFixed(2)).toBe('85.91');
-
-            // Add to cart button click
             await itemsPage.clickCartButton();
 
             // Radio button 
@@ -101,8 +77,7 @@ for ( const csvData of readCSVData()) {
             const totalTip : number = treatValue + tipValue * treatValue;
             expect(totalTip).toBe(3.3);
 
-            await confirmPage.clickRequestButton();
-                        
+            await confirmPage.clickRequestButton();                        
             await itemsPage.assertElementVisible(); 
         }); 
     });
