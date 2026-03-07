2863b8fadc315ed44e08f2999254d9b46f8062c9
VimalaMoger <mgrvmla1@gmail.com>
2026-03-06 20:19:27 -0600

    function format

--- tests/dataDriven/json_data/jsonData.spec.ts
+++ tests/dataDriven/json_data/jsonData.spec.ts
@@ -2,21 +2,6 @@ import { expect } from '@playwright/test';
 import {test} from '../../../fixtures/baseTest';
 import fs from 'fs';
 
-/* 
-  This test script verifies the text input actions on a sample web application.
-            // https://sweet-torte-0bf6bc.netlify.app/  // Sample web app URL
-  It performs the following actions:
-  1. Navigates to the registration page.  
-  2. Fills out the registration form with sample data.
-  3. Submits the registration form and handles the alert dialog.
-  4. Verifies that the login page is displayed after registration.
-  5. Fills out the login form with the registered data.
-  6. Submits the login form.
-  7. Verifies that the items(resource) page is displayed after login.
-  8. Checks the length of the heading text on the items page.
-  9. Radio button and checkbox actions 
-  10. Handles multiple alert dialogs and verifies their messages.
-*/
 
 //test.describe.configure({ mode: 'parallel' });
 //test.use({ actionTimeout: 10000 });
@@ -32,8 +17,7 @@ if (Array.isArray(loginData)) {
      
 
         test.describe(`Grouping multiple tests ${firstName}`, {tag: ['@grouping','@regression']}, async () => {
-            test.beforeEach('Register',async ({registerPage}) => {
-           
+            test.beforeEach('Register',async ({registerPage}) => {           
                 await registerPage.navigateTo('https://sweet-torte-0bf6bc.netlify.app/');
                 await registerPage.assertElementVisible();
                 await registerPage.clickRegisterLink();
@@ -41,43 +25,32 @@ if (Array.isArray(loginData)) {
             });
 
             test('Login', {tag: ['@pageVisible', '@regression']}, async ({ registerPage, loginPage }) => {
-
-                await registerPage.handleAlertDialog();
-              
+                await registerPage.handleAlertDialog();              
                 await loginPage.assertLoginDisplayTextVisible();
-
-                // fill the login form with registered data
-                await loginPage.login(email, password, firstName);
+                await loginPage.login(email, firstName);
             });
 
             test.skip('@browserTest @sanity Browser name test', async ({registerPage}) => {
                 test.skip(await registerPage.getBrowserName() !== 'chromium', 'This test runs only on Chromium browsers');
-                // Test logic specific to Chromium browser
                 console.log('This test is running on Chromium browser');
             });
 
-            //fixme the test
             test.fixme('Fixme test example', async () => {
-                // This test is marked as 'fixme' and will be skipped
-                // until the underlying issue is resolved.
-                // Test logic goes here
+                // This test is marked as 'fixme' and will be skipped                
             });
             
-            // slow test
             //test('slow test example', async () => {
                 //test.slow();
             //});
 
-            //fail the test            
             test.fail('Intentional failure test', async ({registerPage}) => {
                 await registerPage.assertElementVisible(); // This assertion will fail
             });
 
-            test.afterEach('Verify Text Input Actions', async ({ itemsPage, confirmPage }) => { 
-        
-                // Verify resource(items) page displayed after login
+            test.afterEach('Verify Text Input Actions', async ({ itemsPage, confirmPage }) => {         
+                // Verify text visible
                 await itemsPage.assertElementVisible(); 
-                // Verify length of heading text on items page
+                // Verify length of the text
                 const maxLength = await itemsPage.getHeadingTextLength();  
                 expect(maxLength).toBe(22);
 
@@ -86,7 +59,6 @@ if (Array.isArray(loginData)) {
                 const totalItemPrice : number = priceArr.reduce((acc, price) => acc + price, 0);
                 expect(totalItemPrice.toFixed(2)).toBe('85.91');
 
-                // Add to cart button click
                 await itemsPage.clickCartButton();
 
                 // Radio button 
