e76861285aa4d880d2b7e2ad3bf27d593c2490f1
VimalaMoger <mgrvmla1@gmail.com>
2026-03-10 10:48:13 -0500

    Text error correction in tests\datePicker\jQueryDatePicker.spec.ts

--- tests/datePicker/jQueryDatePicker.spec.ts
+++ tests/datePicker/jQueryDatePicker.spec.ts
@@ -2,7 +2,7 @@ import { test as base } from '@playwright/test';
 import { PageThree } from '../../pages/myjquerypage/PageThree';
 
 
-// Extend basic test by providing a "PageThree" fixture
+// Extend Base test by providing a "PageThree" fixture
 const test = base.extend<{ pageThree: PageThree }>({
     pageThree: async ({ page }, use) => {
         const pageThree = new PageThree(page);
@@ -13,14 +13,9 @@ const test = base.extend<{ pageThree: PageThree }>({
 test.only("JQuery DatePicker Test", async ({pageThree}) => {
 
     await pageThree.navigateTo('https://resplendent-pony-e08064.netlify.app/page3');
-
     await pageThree.assertElementVisible();
-
     await pageThree.clickDatePicker();
-
     await pageThree.enterDateValue("2025-11-25");
-
-    //await pageThree.verifyInputValues("2025-11-25");
     await pageThree.verifyTextContent("You selected the date: 2025-11-25");
 });
 
