ac80bcf1781f31ae6b29299a51fbb31df720cac8
Vimala Moger <166641453+VimalaMoger@users.noreply.github.com>
2026-03-12 15:33:18 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests/dataDriven', //'.', //./e2e',
+  testDir: '.', //'.', //./e2e',
   //testMatch:'**/*.spec.ts',
   outputDir: 'test-results/',
   /* Run tests in files in parallel */
