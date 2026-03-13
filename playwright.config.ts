309a40fde832e6147e43d897691faf1366a0f086
Vimala Moger <166641453+VimalaMoger@users.noreply.github.com>
2026-03-13 15:36:43 -0500

    Change test directory to data-driven tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests/dataDriven/twoDArrTest.spec.ts',
+  testDir: './tests/dataDriven/',
   //testMatch:'**/*.spec.ts',
   //outputDir: 'test-results/',
   /* Run tests in files in parallel */
